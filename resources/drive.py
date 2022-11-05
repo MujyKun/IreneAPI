5f4ec279383618c80af7ab80897c764f8fb34c2a
MujyKun <mujy@irenebot.com>
2022-11-05 04:44:10 -0400

    Added queue handling of drive requests to prevent rate limits

--- resources/drive.py
+++ resources/drive.py
@@ -1,4 +1,9 @@
+import asyncio
+import datetime
+import random
 from typing import Optional, List, Dict
+
+import aiogoogle.excs
 from aiogoogle import Aiogoogle
 from aiogoogle.auth.creds import ServiceAccountCreds
 import json
@@ -8,6 +13,59 @@ from dataclasses import dataclass
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 
 
+class Google(Aiogoogle):
+    """Context manager to auto discover the drive api and add manual open/closing."""
+    def __init__(self, creds):
+        super(Google, self).__init__(service_account_creds=creds)
+        self.drive = None
+        self.name = "drive"
+        self.version = "v3"
+
+    async def open(self):
+        return await self.__aenter__()
+
+    async def close(self):
+        return await self.__aexit__(None, None, None)
+
+    async def __aenter__(self):
+        session = self._get_session()
+        if session is None:
+            session = self._set_session()
+            await session.__aenter__()
+            self.drive = await self.discover(self.name, self.version)
+            return self
+        raise RuntimeError("Nesting context managers using the same Aiogoogle object is not allowed.")
+
+
+@dataclass
+class QueueItem:
+    awaitables: callable
+    unique_id = f"{datetime.datetime.now()}-{random.randint(10000, 50000)}"
+    done = False
+    _results = None
+    task = None
+
+    def set_as_done(self):
+        self.done = True
+
+    @property
+    def results(self):
+        return self._results
+
+    def set_results(self, results):
+        self._results = results
+
+
+def exc_handler(loop, context):
+    """Change the handler to ignore unclosed connections as
+    sessions are spammy even while __aexit__ is being called.
+    More Info: https://github.com/aio-libs/aiohttp/issues/5277
+    """
+    if context["message"] == "Unclosed client session":
+        return
+    loop.default_exception_handler(context)
+
+
 class Drive:
     def __init__(self):
         """
@@ -15,40 +73,89 @@ class Drive:
         """
         self.expiry = None
         self.scopes = None
-        self.name = "drive"
-        self.version = "v3"
+        self.initialized = False
         self.__service_creds: Optional[ServiceAccountCreds] = None
+        self.queue = asyncio.Queue()
+
+        # 10 per second is the expected, but the time may be increased if there are ratelimit errors.
+        self.seconds_to_accomplish_max_requests = 2
+        self.max_requests_per_set_time = 10
+        self.google: Optional[Google] = None
+        asyncio.get_event_loop().create_task(self._queue_loop())
+
+    async def _queue_loop(self):
+        item: QueueItem
+        requests_in_time_frame = 0
+        while True:
+            if not self.initialized:
+                await asyncio.sleep(3)
+
+            awaitables = []
+            items = []
+            while not self.queue.empty() and len(awaitables) < self.max_requests_per_set_time:
+                item = await self.queue.get()
+                awaitables.append(item.awaitables)
+                items.append(item)
+
+            requests_in_time_frame += len(items)
+
+            if requests_in_time_frame >= self.max_requests_per_set_time:
+                await asyncio.sleep(self.seconds_to_accomplish_max_requests)
+                requests_in_time_frame = 0
+
+            if not awaitables:
+                await asyncio.sleep(0.5)
+                continue
+
+            results = await asyncio.gather(*awaitables)
+
+            for idx, result in enumerate(results, start=0):
+                item = items[idx]
+                item.set_results(results=result)
+                item.set_as_done()
+
+    async def get_results(self, awaitable):
+        """Run a request with a queue system to adjust for user rate limits."""
+        item = QueueItem(awaitable)
+        await self.queue.put(item)
+        await self._wait_on_queue_item(item)
+        return item.results
+
+    @staticmethod
+    async def _wait_on_queue_item(item: QueueItem):
+        sleep_time = 0
+        while not item.done:
+            await asyncio.sleep(sleep_time)
+            sleep_time += 0.05
 
     async def create(self):
         """Properly initialize this object."""
         async with aiofiles.open("service_account.json", "r") as file:
             service_creds = json.loads(await file.read())
             self.__service_creds = ServiceAccountCreds(**service_creds, scopes=SCOPES)
+            self.google = Google(creds=self.__service_creds)
+            asyncio.get_running_loop().set_exception_handler(exc_handler)
+            self.initialized = True
 
     async def list_files(self):
         """List the files of a Google Drive account."""
-        async with Aiogoogle(service_account_creds=self.__service_creds) as aiogoogle:
-            drive_v3 = await aiogoogle.discover("drive", "v3")
-            json_res = await aiogoogle.as_user(
-                drive_v3.files.list(),
-            )
+        async with self.google as google:
+            json_res = await google.as_service_account(google.drive.files.list())
             for file in json_res["files"]:
                 print(file["name"])
 
     async def download_file(self, file_id, path):
         """Download a google drive file."""
-        async with Aiogoogle(service_account_creds=self.__service_creds) as aiogoogle:
-            drive_v3 = await aiogoogle.discover(self.name, self.version)
-            requests = drive_v3.files.get(
+        async with self.google as google:
+            requests = google.drive.files.get(
                 fileId=file_id, download_file=path, alt="media"
             )
-            results = await aiogoogle.as_service_account(requests)
+            results = await self.get_results((google.as_service_account(requests)))
 
     async def upload_file(self, path):
         """Upload a file to google drive."""
-        async with Aiogoogle(service_account_creds=self.__service_creds) as aiogoogle:
-            drive_v3 = await aiogoogle.discover(self.name, self.version)
-            await aiogoogle.as_service_account(drive_v3.files.create(upload_file=path))
+        async with self.google as google:
+            await self.get_results(google.as_service_account(google.drive.files.create(upload_file=path)))
 
     @staticmethod
     def get_id_from_url(url) -> str:
@@ -61,7 +168,7 @@ class Drive:
         return url.replace("https://drive.google.com/drive/folders/", "")
 
     async def get_nested_files_in_folders(self, folder_id, parent_file=None, faces: Dict[str, int] = None):
-        """Process a Google Drive folder ID recursively.
+        """Process a Google Drive folder ID recursively and concurrently between sub-folders.
 
         :param folder_id: str
             The drive folder ID to recursively get files for.
@@ -72,11 +179,18 @@ class Drive:
             This can be retrieved by calling `convert_os_file_to_faces_dict`
         """
         actual_files = []
+
+        tasks = []
+
+        print(f"Processing Folder ID -> {folder_id}")
         for file in await self.get_files_in_folder(folder_id, parent_file=parent_file):
             if file.is_folder():
-                actual_files += await self.get_nested_files_in_folders(file.id, parent_file=file)
+                tasks.append(self.get_nested_files_in_folders(file.id, parent_file=file))
             else:
                 actual_files.append(file)
+
+        for file_results in await asyncio.gather(*tasks) or []:
+            actual_files += file_results
 
         if not parent_file and faces:  # This means we are back to our root recursively.
             for file in actual_files:
@@ -86,11 +200,10 @@ class Drive:
 
     async def get_files_in_folder(self, folder_id, parent_file=None):
         """Get all files (including other folders) in a folder. """
-        async with Aiogoogle(service_account_creds=self.__service_creds) as aiogoogle:
-            drive_v3 = await aiogoogle.discover(self.name, self.version)
-            requests = drive_v3.files.list(q=f"'{folder_id}' in parents", pageSize=1000)
-            results = await aiogoogle.as_service_account(requests)
-            return File.create_many(results.get("files") or [], parent_file=parent_file)
+        async with self.google as google:
+            requests = google.drive.files.list(q=f"'{folder_id}' in parents", pageSize=1000)
+            results = await self.get_results(google.as_service_account(requests))
+        return File.create_many(results.get("files") or [], parent_file=parent_file)
 
 
 @dataclass
